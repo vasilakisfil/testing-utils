6d98a9207a0987926f9dae183c327bbd161f312f
Filippos Vasilakis <vasilakisfil@gmail.com>
2021-12-07 21:41:19 +0100

    Add list and opt rand helpers, bump to 0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,35 @@ pub trait Randomize {
     fn random() -> Self
     where
         Self: Sized;
+
+    fn rand_list0(range: u32) -> Vec<Self>
+    where
+        Self: Sized,
+    {
+        (0..=rand_num_from(1..=range))
+            .map(|_| Self::random())
+            .collect::<Vec<Self>>()
+    }
+
+    fn rand_list1(range: u32) -> Vec<Self>
+    where
+        Self: Sized,
+    {
+        (1..=rand_num_from(1..=range))
+            .map(|_| Self::random())
+            .collect::<Vec<Self>>()
+    }
+
+    fn list(size: u16) -> Vec<Self>
+    where
+        Self: Sized,
+    {
+        (1..=size).map(|_| Self::random()).collect::<Vec<Self>>()
+    }
+}
+
+pub fn opt<T: Clone>(t: T) -> Option<T> {
+    sample(&[Some(t), None])
 }
 
 impl crate::Randomize for bool {
@@ -24,7 +53,7 @@ pub fn sample<T: Clone, const N: usize>(array: &[T; N]) -> T {
     array[rng.gen_range(0..array.len())].clone()
 }
 
-pub fn sample_vec<T: Clone>(vector: &Vec<T>) -> T {
+pub fn sample_vec<T: Clone>(vector: &[T]) -> T {
     use rand::Rng;
 
     let mut rng = rand::thread_rng();
@@ -66,7 +95,6 @@ where
     rng.gen_range(range)
 }
 
-
 impl Randomize for IpAddr {
     fn random() -> Self {
         IpAddr::V4(Randomize::random())
@@ -80,4 +108,3 @@ impl Randomize for Ipv4Addr {
         Ipv4Addr::new(ip_part(), ip_part(), ip_part(), ip_part())
     }
 }
-
